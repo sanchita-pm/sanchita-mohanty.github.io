eed87b5df8e2991b93e2fb4f6bd45cf0e25901d0
Sanchita Mohanty <mohanty12sanchita@gmail.com>
2025-07-17 12:19:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Preloader from "../src/components/Loader";
+import Preloader from "./src/components/Loader";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
